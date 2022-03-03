629e986824f65826230107a195dbcbd0cadc1230
Narayana <458narayana@gmail.com>
2022-03-03 12:27:05 +0000

    first commit2

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://amazingkart.herokuapp.com/']
 
 
 # Application definition
